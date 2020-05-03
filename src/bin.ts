4a1771ff819312a49bd4c8278e6801e3bd82f547
Pedro Durek <pedro_hmd@hotmail.com>
2020-05-02 22:42:16 -0500

    Move convert helper to its appropriate place

--- src/bin.ts
+++ src/bin.ts
@@ -1,50 +1,19 @@
 import inquirer from 'inquirer';
-import path from 'path';
-import GenerateKeys from './generate/generateKeys';
-import { generateTemplate } from './generate/generateTypes';
+// import path from 'path';
+// import { generateOutput } from './generate/generateTypes';
+import { getQuestion } from './questions';
+import { addScriptToPackageFile, generateConfig } from './init';
 
 const cmd = process.argv[2];
 
-type Resource =
-  | string
-  | string[]
-  | {
-      [key: string]: Resource;
-    };
-
-const convert = (obj: Resource) => {
-  if (typeof obj !== 'string' && !Array.isArray(obj)) {
-    const [lng] = Object.keys(obj);
-    const result = Object.entries(obj[lng]).map(
-      ([key, value]) => new GenerateKeys(key, value),
-    );
-    console.log(generateTemplate(result));
-  }
-};
-
 const setup = () => {
-  inquirer
-    .prompt([
-      {
-        type: 'input',
-        name: 'resources',
-        message: 'Where is your resources file?:',
-        default: 'i18n/resources.js',
-        // default: "src/i18n/resources.ts",
-        validate: (str: string) => str.length > 0,
-      },
-      {
-        type: 'input',
-        name: 'output',
-        message: 'Where to write the output?:',
-        default: 'src/i18n/types.ts',
-        validate: (str: string) => str.length > 0,
-      },
-    ])
-    .then((answer) => {
-      const url = path.resolve(__dirname, answer.resources);
-      import(url).then((result) => convert(result.default));
-    });
+  inquirer.prompt(getQuestion()).then(({ script, config, ...rest }) => {
+    // const test = YAML.stringify(answer);
+    addScriptToPackageFile(script, config);
+    generateConfig(config, rest);
+    // const url = path.resolve(__dirname, answer.resources);
+    // import(url).then((result) => generateOutput(result.default));
+  });
 };
 
 switch (cmd) {
