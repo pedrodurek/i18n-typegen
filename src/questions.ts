8bfee8632a822f4e877ec183f4134fe25b199e53
Pedro Durek <pedro_hmd@hotmail.com>
2020-05-02 22:42:47 -0500

    Add questions file

---
+++ src/questions.ts
@@ -0,0 +1,31 @@
+export const getQuestion = () => [
+  {
+    type: 'input',
+    name: 'resources' as const,
+    message: 'Where is your resources file?',
+    default: 'i18n/resources.js',
+    // default: "src/i18n/resources.ts",
+    validate: (str: string) => str.length > 0,
+  },
+  {
+    type: 'input',
+    name: 'output' as const,
+    message: 'Where to write the output?',
+    default: 'src/i18n/types.ts',
+    validate: (str: string) => str.length > 0,
+  },
+  {
+    type: 'input',
+    name: 'config' as const,
+    message: 'How to name the config file?',
+    default: 'i18ngen.yml',
+    validate: (str: string) =>
+      str.length > 0 && str.toLocaleLowerCase().endsWith('.yml'),
+  },
+  {
+    type: 'input',
+    name: 'script' as const,
+    message: 'What script in package.json should run the i18ngen?',
+    validate: (str: string) => str.length > 0,
+  },
+];
