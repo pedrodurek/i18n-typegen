489ca2a4f36c634698c0b5f2ae1f13bfc0762cc1
Pedro Durek <pedro_hmd@hotmail.com>
2020-05-15 00:15:25 -0500

    Move generateOutput to index and add writeTypes to read resources file and generate all types

---
+++ src/generate/index.ts
@@ -0,0 +1,25 @@
+import YAML from 'yamljs';
+import { resolve } from 'path';
+import { Resource } from './types';
+import GenerateKeys from './generateKeys';
+import { generateTemplate } from './generateTypes';
+
+const generateOutput = (obj: Resource) => {
+  if (typeof obj !== 'string' && !Array.isArray(obj)) {
+    const [lng] = Object.keys(obj);
+    const result = Object.entries(obj[lng]).map(([key, value]) => {
+      return new GenerateKeys(key, value);
+    });
+    return generateTemplate(result);
+  }
+  // throw error - invalid format
+  return null;
+};
+
+export const writeTypes = async (configPath: string) => {
+  const path = resolve(process.cwd(), configPath);
+  const config = YAML.load(path);
+  const url = resolve(__dirname, config.resources);
+  const result = await import(url);
+  generateOutput(result.default);
+};
